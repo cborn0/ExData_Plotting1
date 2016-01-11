d46deb7b3d623c3d776d90f1fa0afd82bcd0b19b
cborn0 <cborn@swbell.net>
2016-01-10 23:01:29 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 power <- read.csv(file="C://Users//cborn0//Google Drive//Coursera//household_power_consumption.txt",head=TRUE,sep=";")
 power$Date <- as.Date(power$Date, "%d/%m/%Y")
 power <- power[power$Date == as.Date("2007-02-01") | power$Date == as.Date("2007-02-02"),]
-power$Global_active_power <- as.numeric(as.character((power$Global_active_power))
+power$Global_active_power <- as.numeric(as.character((power$Global_active_power)))
 
 hist(power$Global_active_power,main = "Global Active Power" ,xlab = "Global Active Power (kilowatts)", col = "red", breaks = 11)
