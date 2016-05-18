9373ea4627449947ebbfc77866fc9236dc0ef757
cborn0 <cborn@swbell.net>
2016-05-18 11:47:31 -0500

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -3,4 +3,5 @@ power$Date <- as.Date(power$Date, "%d/%m/%Y")
 power <- power[power$Date == as.Date("2007-02-01") | power$Date == as.Date("2007-02-02"),]
 power$Global_active_power <- as.numeric(as.character((power$Global_active_power)))
 
+
 hist(power$Global_active_power,main = "Global Active Power" ,xlab = "Global Active Power (kilowatts)", col = "red", breaks = 11)
